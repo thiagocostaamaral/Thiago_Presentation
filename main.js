b650a2b1e8244fac6e1df367e1cb1cf8a7785c65
thiago.amaral <thiagocosta.a@hotmail.com>
2022-01-17 19:38:22 -0300

    Inclusion of javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+projects = {
+    'Project 1':{
+        'Image':'',
+        'Description':'',
+    },
+    'Project 1':{
+        'Image':'',
+        'Description':'',
+    },
+    'Project 1':{
+        'Image':'',
+        'Description':'',
+    },
+    'Project 1':{
+        'Image':'',
+        'Description':'',
+    },
+}
+
+document.getElementById('Projects').innerHTML = 'My projects'
